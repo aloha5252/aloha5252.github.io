4e37a3bec9a217982b122e00d58e306d95ec09eb
aloha5252 <107113514+aloha5252@users.noreply.github.com>
2022-06-10 21:52:47 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,7 +1,21 @@
-$(function(){
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// document.querySelector('.top_banner i').addEventListener('click', bannerClose);
 
+// var bannerClose = function () {
+//     document.querySelector('top_banner').style.display = 'none';
+// }
 
+$(function () {
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-});
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp();
+    });
+
+    $('.main_slider').slick({
+        autoplay: true,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+
+    });
+
+})
