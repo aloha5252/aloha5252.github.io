9b84da08864856686c7d29b9fac6aa16236aff65
aloha5252 <107113514+aloha5252@users.noreply.github.com>
2022-06-15 21:46:10 +0900

    Add files via upload

---
+++ trn01/js/portfolio.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('.section').eq(0).addClass('on');
+
+    $('#portfolio').fullpage({
+        anchors: ['p01', 'p02', 'p03', 'p04', 'p05'],
+        afterLoad: function (anchorLink, index) {
+            console.log(anchorLink, index);
+
+            $('.section').eq(index - 1).addClass('on').siblings().removeClass('on');
+            $('.nb li').eq(index - 1).addClass('on').siblings().removeClass('on');
+            // var _this = $('.active');
+            // _this.addClass('on').siblings().removeClass('on')
+        },
+    });
+
+    $('.cover_open').on('click', function () {
+        $('#cover').toggleClass('on')
+    });
+
+    $('#cover a').on('click', function () {
+        $('#cover').removeClass('on');
+    });
+
+});
